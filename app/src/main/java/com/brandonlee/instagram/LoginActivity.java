e03511d8ed66b8f9ef9b90bcdd076350fa7e78c8
blee062 <blee062@ucr.edu>
2018-01-18 12:26:58 -0800

    Test

--- app/src/main/java/com/brandonlee/instagram/LoginActivity.java
+++ app/src/main/java/com/brandonlee/instagram/LoginActivity.java
@@ -9,5 +9,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        test();
     }
+    public void test() {
+        return;
+    }
+
 }
