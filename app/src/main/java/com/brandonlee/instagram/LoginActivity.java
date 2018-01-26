fe388bb48796dff887354df26bc5e4e0c65b9d85
brandonlee0405 <brandonlee0405@yahoo.com>
2018-01-26 15:46:50 -0800

    Test brandon linux

--- app/src/main/java/com/brandonlee/instagram/LoginActivity.java
+++ app/src/main/java/com/brandonlee/instagram/LoginActivity.java
@@ -1,20 +1,28 @@
 package com.brandonlee.instagram;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        test();
+        findViewById(R.id.tvSignUp).setOnClickListener(this);
     }
-    public void test() {
-        return;
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.tvSignUp:
+
+                startActivity(new Intent(this, SignUpActivity.class));
+
+                break;
+        }
     }
-    //hello
-    // hello brandon
 }
--- app/src/main/java/com/brandonlee/instagram/SignUpActivity.java
+++ app/src/main/java/com/brandonlee/instagram/SignUpActivity.java
@@ -1,13 +1,89 @@
 package com.brandonlee.instagram;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class SignUpActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText etEmail, etPassword;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+
+    private void registerUser() {
+        String email = etEmail.getText().toString().trim();
+        String password = etPassword.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            etEmail.setError("Email is required");
+            etEmail.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            etEmail.setError("Please enter a valid email");
+            etEmail.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            etPassword.setError("Password is required");
+            etPassword.requestFocus();
+            return;
+        }
+
+        if (password.length() < 6) {
+            etPassword.setError("Minimum length of passwrod should be 6");
+            etPassword.requestFocus();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "User Registered Successful", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnSignUp:
+                registerUser();
+                break;
+
+            case R.id.tvLogin:
+                startActivity(new Intent(this, LoginActivity.class));
+                break;
+        }
     }
 }
